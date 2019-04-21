9d5ea3177ebfb1f7785a5aa36de5b0e7ed85e667
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-04-21 20:35:05 +0700

    Add husky and lint staged

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,30 @@
 module.exports = {
   siteMetadata: {
-    title: `Rahman Fadhil`,
-    description: `Full-Stack Mobile & Web Developer`,
-    author: `@rahmanfadhil`,
+    title: 'Rahman Fadhil',
+    description: 'Full-Stack Mobile & Web Developer',
+    author: '@rahmanfadhil',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
